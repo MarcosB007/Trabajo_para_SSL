a4a54fd22a8864a065c344a7764229ba708b5cf5
Marcos Brandan <Marcos.Brandan@alu.frt.utn.edu.ar>
2021-05-09 11:26:20 -0300

    crear funcion Carga_ISBN y agregar validaciones

--- EjercicioAplicacio.cpp
+++ EjercicioAplicacio.cpp
@@ -18,3 +18,24 @@ main(){
 	
 	
 }
+
+void carga_ISBN(FILE *archivo, ISBN isbn){
+	int contador = 13;
+	printf("Ingrese el prefijo internacional (3 digitos): ");
+	scanf("%d", isbn.prefijo_internacional);
+	contador-=isbn.prefijo_internacional;
+	if(isbn.prefijo_internacional == 978 || isbn.prefijo_internacional == 979){
+		printf("Ingrese el identificador de grupo (1 a 5 digitos, %d digitos restantes): ", contador);
+		scanf("%d", isbn.grupo_registro);
+		contador-=isbn.grupo_registro;
+		if(isbn.grupo_registro > 0 && isbn.grupo_registro < 6){
+			printf("Ingrese el prefijo de editor (1 a 7 digitos, %d digitos restantes)", contador);
+		}
+	}
+	
+}
+
+
+
+
+
